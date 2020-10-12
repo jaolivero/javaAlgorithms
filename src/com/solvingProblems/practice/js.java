5888882ced74e2b0cc90199b374dc852e56ac7f1
Joel Olivero <joelolivero@outlook.com>
2020-10-11 23:48:49 -0400

    working

--- src/com/solvingProblems/practice/js.java
+++ src/com/solvingProblems/practice/js.java
@@ -2,7 +2,7 @@ package com.solvingProblems.practice;
 
 public class js {
     public void NewYearsChaosP() {
-        int [] line = {5,}
+        int [] line = {5,2, 1,3,4}
         int swap = 0;
         int bribes;
         int pos = 0;
