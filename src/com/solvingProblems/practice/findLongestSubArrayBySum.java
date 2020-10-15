acb8fcf0481d4b732f5514b4a1d84d2a451d6335
Joel Olivero <joelolivero@outlook.com>
2020-10-15 09:54:15 -0400

    used a sliding window algorithom to solve the problem

---
+++ src/com/solvingProblems/practice/findLongestSubArrayBySum.java
@@ -0,0 +1,20 @@
+package com.solvingProblems.practice;
+
+public class findLongestSubArrayBySum {
+    public int [] findFunction(int s, int[] arr) {
+        int[] result = new int[]{-1};
+
+        int sum = 0;
+        int left = 0;
+        int right = 0;
+
+        while (right < arr.length) {
+            sum += arr[right];
+            while (left < right && sum > s) {
+                sum -= arr[left++];
+            }
+            if (sum == s && (result.length == 1 || result[1] - result[0] < right - left)) ;
+        }
+        return result;
+    }
+}
